d92fc7049e87353d9283e16d65ef78d54d587f32
Jannchie <jannchie@gmail.com>
2021-03-12 02:25:35 +0900

    feat: row & col

---
+++ src/components/JRow.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row q-col-gutter-sm">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
