0c16ee8851e556abbeea67d62e78e0dee12caaef
Jannchie <jannchie@gmail.com>
2021-03-11 17:24:12 +0900

    style: chart style

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,9 +1,87 @@
 <template>
   <q-page padding :style-fn="myTweak">
-    <div class="fit column">
-      <div class="col">
-        <div class="row fit q-col-gutter-sm">
-          <div class="col-8">
+    <div class="row fit q-col-gutter-sm">
+      <div class="col-12 col-sm-8 column q-col-gutter-sm">
+        <q-scroll-area class="col q-pt-none q-mt-sm">
+          <div class="q-pa-md bg-grey-2 rounded-borders">
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
+            <q-chat-message
+              name="me"
+              avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+              :text="['hey, how are you?']"
+              sent
+              stamp="7 minutes ago"
+            />
+            <q-chat-message
+              name="Jane"
+              avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              :text="[`doing fine, how r you?`]"
+              stamp="4 minutes ago"
+            />
             <q-chat-message
               name="me"
               avatar="https://cdn.quasar.dev/img/avatar4.jpg"
@@ -18,40 +96,40 @@
               stamp="4 minutes ago"
             />
           </div>
-          <div class="col-4">
-            <q-scroll-area class="fit bg-grey-2">
-              <q-list>
-                <q-item v-for="i in 50" :key="i" clickable v-ripple>
-                  <q-item-section avatar>
-                    <q-avatar font-size="52px" color="teal" text-color="white">
-                      <img src="https://cdn.quasar.dev/img/avatar3.jpg" />
-                    </q-avatar>
-                  </q-item-section>
-                  <q-item-section>name</q-item-section>
-                </q-item>
-              </q-list>
-            </q-scroll-area>
-          </div>
-        </div>
-      </div>
-      <div class="col-auto">
-        <div class="column q-col-gutter-sm">
-          <div class="col">
-            <q-input outlined v-model="text" outline autogrow />
-          </div>
-          <div class="col">
-            <div class="row">
-              <q-space />
-              <q-btn
-                class="col-12 col-lg-1 col-md-2"
-                color="primary"
-                push
-                icon="check"
-                label="OK"
-              />
+        </q-scroll-area>
+        <div class="col-auto">
+          <div class="column q-col-gutter-sm">
+            <div class="col">
+              <q-input outlined v-model="text" outline autogrow />
+            </div>
+            <div class="col">
+              <div class="row">
+                <q-space />
+                <q-btn
+                  style="max-width: 50px"
+                  class="col-12 col-sm-2"
+                  color="primary"
+                  push
+                  icon="send"
+                />
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="col-12 col-sm-4">
+        <q-scroll-area class="fit bg-grey-2 rounded-borders">
+          <q-list>
+            <q-item v-for="i in 50" :key="i" clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar font-size="52px" color="teal" text-color="white">
+                  <img src="https://cdn.quasar.dev/img/avatar3.jpg" />
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>name</q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
       </div>
     </div>
   </q-page>
